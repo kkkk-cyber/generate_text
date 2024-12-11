d0fdcbff7825a8d15302b71359a682eca3266efa
Faragola <59463606+natashafreda@users.noreply.github.com>
2024-12-11 18:28:25 +0800

    Update llama_xformers_attn_monkey_patch.py

--- generate_text/llama_xformers_attn_monkey_patch.py
+++ generate_text/llama_xformers_attn_monkey_patch.py
@@ -15,7 +15,6 @@ except ImportError:
 def replace_llama_attn_with_xformers_attn():
     transformers.models.llama.modeling_llama.LlamaAttention.forward = xformers_forward
 
-
 def xformers_forward(
     self,
     hidden_states: torch.Tensor,
