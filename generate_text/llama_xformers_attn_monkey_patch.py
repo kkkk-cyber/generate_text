1dc51d1aef68e8489ab140220a4f653fe35ed5dd
Faragola <59463606+natashafreda@users.noreply.github.com>
2024-12-11 18:27:53 +0800

    Update llama_xformers_attn_monkey_patch.py

--- generate_text/llama_xformers_attn_monkey_patch.py
+++ generate_text/llama_xformers_attn_monkey_patch.py
@@ -4,7 +4,6 @@ Directly copied the code from https://raw.githubusercontent.com/oobabooga/text-g
 import logging
 import math
 from typing import Optional, Tuple
-
 import torch
 import transformers.models.llama.modeling_llama
 from torch import nn
