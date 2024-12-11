4569f05fc4550073544ded0028b61704ce0c1021
Faragola <59463606+natashafreda@users.noreply.github.com>
2024-12-11 18:33:24 +0800

    Update llama_xformers_attn_monkey_patch.py

--- generate_text/llama_xformers_attn_monkey_patch.py
+++ generate_text/llama_xformers_attn_monkey_patch.py
@@ -106,7 +106,6 @@ def xformers_forward(
                 f" {attn_output.size()}"
             )
         attn_output = attn_output.transpose(1, 2)
-
     attn_output = attn_output.reshape(bsz, q_len, self.hidden_size)
     attn_output = self.o_proj(attn_output)
     return attn_output, attn_weights, past_key_value
